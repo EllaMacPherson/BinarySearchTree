cbbf2aa7bd8ea9b76130242091d5493e4ca6a3d4
EllaMacPherson <ella.mm.macpherson@gmail.com>
2026-02-20 18:52:39 -0800

    pretty sure add func works, thinking through print func

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "node.h"
 
 using namespace std;
@@ -6,19 +7,65 @@ using namespace std;
 // Tree building
 void insert(node* current, node*& root, int value);
 
+void print(node* current);
+
 int main(){
 
-  int input = 0;
+  int input = 0; // Number to be added currently
+
+  string command = ""; // What the user wants to do
   node* root = NULL; // Root of the tree
 
-  cout<<"Enter a number you'd like to add to the tree"<<endl;
-  cin>>input;
+  while(true){
 
-  // Add it to the tree
-  insert(root, root, input);
+    cout<<"Enter ADD, FILE ADD"<<endl;
 
-  cout<<root->value<<endl;
+    getline(cin, command);
+    
+    if(command == "FILE ADD" || command == "file add"){
+      string filename = ""; // Stores file name
+
+      cout<<"Please enter name of file: "<<endl;
+      getline(cin, filename);
+    
+      ifstream inputFile;
+      inputFile.open(filename);
+    
+      if(!inputFile.is_open()){
+	cout<<"file doesnt exist"<<endl;
+      }
+      else{
+	while(inputFile >> input){
+	  // Add it to the tree
+	  insert(root, root, input);
+	}
+      }
+    }
+    
+    if(command == "add" || command == "ADD"){
+      cout<<"Enter number to add"<<endl;
+      cin>>input;
+      insert(root, root, input);
+    }
+    
+    if(command == "print" || command == "PRINT"){
+      cout<<"ROOT: "<< root->value<<endl;
+      if(root->right != NULL){
+	cout<<"ROOT RIGHT: " <<root->right->value<<endl;
+      }
+      if(root->left != NULL){
+	cout<<"ROOT LEFT: " <<root->left->value<<endl;
+      }
+
+      print(root);
+    }    
+
+  }
   
+}
+
+void print(node* current){
+  cout<<"running real pring func"<<endl;
 }
 
 void insert(node* current, node*& root, int value){
@@ -27,5 +74,39 @@ void insert(node* current, node*& root, int value){
   if(root == NULL){
     // Create a root node
     root = new node(NULL, NULL, value);
+    return;
   }
+
+  /*  // If the current node has no left or right
+  if(current->right == NULL && current->left == NULL){
+    // Check if value is greater than current value
+    if(current->value < value){
+     
+    }
+    // If value is less than current value
+    else{ 
+      current->left = new node(NULL,NULL,value);
+    }
+  }*/
+
+  if(current->value < value){
+    // recursive down to right till NULL
+    if(current->right != NULL){
+      insert(current->right, root, value);
+    }
+    else{
+      current->right = new node(NULL, NULL, value);
+    }
+  }
+
+  if(current->value >  value){
+    //recursive down to left till NULL
+    if(current->left != NULL){
+      insert(current->left, root, value);
+    }
+    else{
+      current->left = new node(NULL, NULL, value);
+    }
+  }
+  
 }
